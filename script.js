f8b2d114f340402d1ded149c4fbd596175b34fba
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2022-05-18 21:42:36 +0530

    ✨ Add `?` support and formatting

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const redirects = {
     "profile": "https://scratch.mit.edu/users/Chiroyce/"
-}
+};
 
-const hash = location.href.split("#")[1];
+const hash = location.href.split("#")[1] || location.href.split("?")[1];
 
 const heading = (t) => document.querySelector('#heading').innerText = t;
 const text = (t) => document.querySelector('#text').innerText = t;
 
-if (hash in redirects){
+if (hash in redirects) {
     heading("You are being redirected to");
     text(redirects[hash]);
     setTimeout(() => location.href = redirects[hash], 500)
 } else {
     heading("404, Not Found");
     text("The requested page was not found.")
-    document.title = "404, Not Found"
+    document.title = "404, Not Found";
 }
